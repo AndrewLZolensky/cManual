a603d672de3f1b9b8aced1962db3fd41a4f137c7
Andrew Zolensky <andrewlzolensky@gmail.com>
2023-12-30 16:59:12 -0600

    wrote a demonstration of scope and how it interacts with declarations and definitions

--- scopeAndLinkage/scope.c
+++ scopeAndLinkage/scope.c
@@ -2,5 +2,24 @@
 #include <stdlib.h>
 
 int main() {
+
+    // if we re-assign but do not re-define "a" in the nested block, it will
+    // update the value outside of the scope of the block
+    int a = 4;
+    {
+        a = 7;
+        printf("a = %d\n", a);
+    }
+    printf("a = %d\n", a);
+
+    // but if we do re-define AND re-assign it, the scope of the variable
+    // is merely the inside of the nested block
+    int b = 4;
+    {
+        int b = 7;
+        printf("b = %d\n", b);
+    }
+    printf("b = %d\n", b);
+
     return(0);
 }
